97449a8227b1d250b271aa460e507c58653c5d16
pradeep1714168 <61132861+pradeep1714168@users.noreply.github.com>
2020-02-17 12:29:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+random.randint(1,6)
+user='yes'
+while user=='yes'or user=='y'or user=='YES'or user=='Yes':
+    print(random.randint(1,6))
+    user=input("play again press yes or No:")
+else:
+    user='NO'or user=='N'or user=='no'
+    print("Thank you")
+
+ 
+
+
+
+    
